4abaffd07bf3075d8499c4718c7b39e4efc2b3cf
Jimmy PIERRAT <jimmy.pierrat@gmail.com>
2018-12-05 22:32:19 +0100

    Ajout de la logique des rôles et de la sécurité

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Ad;
 use Faker\Factory;
+use App\Entity\Role;
 use App\Entity\User;
 use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -22,6 +23,23 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create("FR-fr");
+
+        $adminRole = new Role();
+        $adminRole->setTitle('ROLE_ADMIN');
+        $manager->persist($adminRole);
+
+        $adminUser = new User();
+        $adminUser->setFirstName( 'Pierrat' )
+                  ->setLastName( 'Jimmy' )
+                  ->setEmail( 'jimmy.pierrat@gmail.com')
+                  ->setIntroduction( $faker->sentence())
+                  ->setDescription('<p>' . join('</p><p>', $faker->paragraphs(3) ) . '</p>')
+                  ->setHash($this->encoder->encodePassword($adminUser, 'password'))
+                  ->setPicture('https://avatars.io/twitter/JimmyPrrt')
+                  ->addUserRole($adminRole);
+        
+        $manager->persist($adminUser);
+
 
         // Nous gérons les utilisateurs
         $users = [];
