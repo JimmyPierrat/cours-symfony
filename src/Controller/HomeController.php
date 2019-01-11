ab19721fb4344e9fb7a803ed56073e9656fa8ad7
Jimmy PIERRAT <jimmy.pierrat@gmail.com>
2019-01-11 21:49:42 +0100

    Mise en place de Encore pour nos CSS et JS

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,31 +2,22 @@
 
 namespace App\Controller;
 
+use App\Repository\AdRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class HomeController extends Controller {
-
-
-    /**
-     * @Route("/hello/{prenom}", name="hello")
-     * Montre la page qui dit bonjour
-     *
-     * @return void
-     */
-    public function hello($prenom = "anonyme") {
-        return new Response("Bonjour..." . $prenom);
-    }
-
     /**
      * @Route("/", name="homepage")
      */
-    public function home() {
+    public function home(AdRepository $adRepo, UserRepository $userRepo) {
         return $this->render(
             'home.html.twig',
             [
-                'title' => "Bonjour à tous !"
+                'ads' => $adRepo->findBestAds(3),
+                'users' => $userRepo->findBestUsers()
             ]
         );
     }
